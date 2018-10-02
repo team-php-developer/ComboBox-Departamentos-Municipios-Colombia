7d70727734a6647d22bfab0283445a4e01b7bfa7
Aldair Morales Cuellar <al-dair12@hotmail.es>
2018-10-01 19:00:17 -0500

    actualización
    se actualiza la base de datos y se hacen los registros en el sistema.

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,6 @@
 <?php 
 function getConn(){
-	$mysqli = mysqli_connect('localhost', 'root', 'ingmorales', "database_educate");
+	$mysqli = mysqli_connect('localhost', 'root', 'ingmorales', "departamentos_municipios");
 	if (mysqli_connect_errno($mysqli))
 		echo "Error al Conectar la Base de Datos: " . mysqli_connect_error();
 	$mysqli->set_charset('utf8');
